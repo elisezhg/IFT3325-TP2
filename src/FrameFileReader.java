4183197ee454cffce4ade06381c61626a5d1b60c
_elise_ <elise.zheng@umontreal.ca>
2021-11-18 10:30:46 -0500

    modified getNextFrame's logic

--- src/FrameFileReader.java
+++ src/FrameFileReader.java
@@ -1,17 +1,24 @@
 package src;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class FrameFileReader {
-    private FileReader reader;
+    public static int FRAME_ENCODING_BITS = 3;
+
+    private int counter = 0;
+    // private FileReader reader;
+    private BufferedReader reader;
+
     public FrameFileReader(String filename) throws FileNotFoundException{
-	reader = new FileReader(filename);
+        // reader = new FileReader(filename);
+        reader = new BufferedReader(new FileReader(filename));
     }
 
     public void closeFile() throws IOException{
-	reader.close();
+        reader.close();
     }
 
     /**
@@ -25,22 +32,39 @@ public class FrameFileReader {
      * @return read frame
      */
     public CharFrame getNextFrame(String polynomial) throws IOException, InvalidFrameFileException{
-	if(reader.read() != CharFrame.FLAG_CHAR)
-	    throw new InvalidFrameFileException();
+        if(reader.read() != CharFrame.FLAG_CHAR)
+            throw new InvalidFrameFileException();
 
-	int type = reader.read();
-	int num = reader.read();
+        int type = reader.read();
+        int num = reader.read();
 
-	StringBuilder data = new StringBuilder();
-	for (int d = reader.read(); d != CharFrame.FLAG_CHAR; d = reader.read()) {
-	    if(d < 0)//unexpected EOF
-		throw new InvalidFrameFileException();
-	    data.append((char) d);
-	}
+        StringBuilder data = new StringBuilder();
+        for (int d = reader.read(); d != CharFrame.FLAG_CHAR; d = reader.read()) {
+            if(d < 0)//unexpected EOF
+                throw new InvalidFrameFileException();
+            data.append((char) d);
+        }
 
-	if(reader.read() != '\n')
-	    throw new InvalidFrameFileException();
+        if(reader.read() != '\n')
+            throw new InvalidFrameFileException();
 
         return new CharFrame((char)type,(char) num, data.toString(), polynomial);
+    }
+
+    // Reads the next line from the open file and constructs a new frame
+    public CharFrame getNextFrame_() throws IOException{
+
+        String line = reader.readLine();
+
+        // EOF
+        if (line == null) {
+            reader.close();
+            return null;
+        }
+
+        CharFrame frame = new CharFrame('I', (char) this.counter, line, null);
+        this.counter = (this.counter + 1) % (int) Math.pow(2, FRAME_ENCODING_BITS);
+
+        return frame;
     }
 }
