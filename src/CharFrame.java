a8de33e75fbdcf75eefe30951085e43a73e70c15
_elise_ <elise.zheng@umontreal.ca>
2021-11-27 09:38:55 -0500

    Moved CRC validation and changed getData()
    -The validation portion in the constructor has been moved into isValid()
    - getData() returns the string representation of the bitstring

--- src/CharFrame.java
+++ src/CharFrame.java
@@ -18,11 +18,11 @@ public class CharFrame {
      * @return str padded with '0' to the left to reach target length
      */
     private static String padLeft(String str, char pad, int targetLength){
-	StringBuilder result = new StringBuilder(str);
-	while(result.length() < targetLength){
-	    result.insert(0, pad);
-	}
-	return result.toString();
+        StringBuilder result = new StringBuilder(str);
+        while(result.length() < targetLength){
+            result.insert(0, pad);
+        }
+        return result.toString();
     }
     //PUBLIC METHODS
     /**
@@ -31,14 +31,14 @@ public class CharFrame {
      * @param polynomial polynomial to be used for checksum in bitstring
      */
     public CharFrame( char type, String data, String polynomial) {
-	this.type = padLeft(Integer.toBinaryString(type), '0', TYPE_BITSIZE);
-	StringBuilder dataBits = new StringBuilder();
-	for(int i = 0; i < data.length(); i++)
-	    dataBits.append(padLeft(Integer.toBinaryString(data.charAt(i)), '0', 8));
-	this.data = dataBits.toString();
-	this.polynomial = polynomial;
+        this.type = padLeft(Integer.toBinaryString(type), '0', TYPE_BITSIZE);
+        StringBuilder dataBits = new StringBuilder();
+        for(int i = 0; i < data.length(); i++)
+            dataBits.append(padLeft(Integer.toBinaryString(data.charAt(i)), '0', 8));
+        this.data = dataBits.toString();
+        this.polynomial = polynomial;
 
-	this.num = null;
+        this.num = null;
     }
     /**
      * @param frame formated frame (bitstring)
@@ -46,82 +46,106 @@ public class CharFrame {
      * @throws BadTransmissionException if frame has invalid flags or checksum
      */
     public CharFrame(String frame, String polynomial) throws InvalidFrameException{
-	if(!frame.substring(0, FLAG.length()).equals(FLAG) || !frame.substring(frame.length() - FLAG.length()).equals(FLAG)){
-	    //if flag is not found at start and end of frame
-	    throw new InvalidFrameException();
-	}
-	String content = BitStuffer.destuff(frame.substring(FLAG.length(), frame.length() - FLAG.length()));
-	if(!CheckSumCalculator.validate(content, polynomial)){
-	    //if checksum is invalid
-	    throw new InvalidFrameException();
-	}
-	this.type = content.substring(0, TYPE_BITSIZE);
-	this.num = content.substring(TYPE_BITSIZE, TYPE_BITSIZE + NUM_BITSIZE);
-	this.data = content.substring(TYPE_BITSIZE + NUM_BITSIZE, content.length() - CRC_BITSIZE);
-	this.polynomial = polynomial;
+        if(!frame.substring(0, FLAG.length()).equals(FLAG) || !frame.substring(frame.length() - FLAG.length()).equals(FLAG)){
+            //if flag is not found at start and end of frame
+            throw new InvalidFrameException();
+        }
+        String content = BitStuffer.destuff(frame.substring(FLAG.length(), frame.length() - FLAG.length()));
+
+        // Moved into isValid()
+        // if(!CheckSumCalculator.validate(content, polynomial)){
+        //     //if checksum is invalid
+        //     throw new InvalidFrameException();
+        // }
+        
+        this.type = content.substring(0, TYPE_BITSIZE);
+        this.num = content.substring(TYPE_BITSIZE, TYPE_BITSIZE + NUM_BITSIZE);
+        this.data = content.substring(TYPE_BITSIZE + NUM_BITSIZE, content.length() - CRC_BITSIZE);
+        this.polynomial = polynomial;
 
     }
 
     public char getType(){
-	return (char) Integer.parseInt(type, 2);
+        return (char) Integer.parseInt(type, 2);
     }
+
     public void setType(char type){
-	this.type = padLeft(Integer.toBinaryString(type), '0', TYPE_BITSIZE);
+        this.type = padLeft(Integer.toBinaryString(type), '0', TYPE_BITSIZE);
     }
+
     public int getNum(){
-	return Integer.parseInt(num, 2);
+        return Integer.parseInt(num, 2);
     }
+
     public void setNum(int num){
-	String numstr = Integer.toBinaryString(num);
-	if(NUM_BITSIZE < numstr.length()){
-	    throw new IllegalArgumentException("arg int num is not small enough to be represented over " + NUM_BITSIZE + " bits");
-	}
-	this.num = padLeft(numstr, '0', NUM_BITSIZE);
+        String numstr = Integer.toBinaryString(num);
+        if(NUM_BITSIZE < numstr.length()){
+            throw new IllegalArgumentException("arg int num is not small enough to be represented over " + NUM_BITSIZE + " bits");
+        }
+        this.num = padLeft(numstr, '0', NUM_BITSIZE);
     }
+
     public String getData(){
-	return data;
+        StringBuilder str = new StringBuilder();
+
+        for (int i = 0; i < this.data.length(); i+=8) {
+            char c = (char) Integer.parseInt(data.substring(i, i + 8), 2);
+            str.append(c);
+        }
+
+        return str.toString();
     }
 
     /**
      * @param data as plain text
      */
     public void setData(String data){
-	StringBuilder dataBits = new StringBuilder();
-	for(int i = 0; i < data.length(); i++)
-	    dataBits.append(padLeft(Integer.toBinaryString(data.charAt(i)), '0', 8));
-	this.data = dataBits.toString();
+        StringBuilder dataBits = new StringBuilder();
+        for(int i = 0; i < data.length(); i++)
+            dataBits.append(padLeft(Integer.toBinaryString(data.charAt(i)), '0', 8));
+        this.data = dataBits.toString();
     }
 
     public String getPolynomial() {
-	return polynomial;
+        return polynomial;
     }
 
     public void setPolynomial(String polynomial) {
-	this.polynomial = polynomial;
+        this.polynomial = polynomial;
     }
     /**
      * computes checksum for the frame
      * @return checksum
      */
     public String computeCRC() {
-	return CheckSumCalculator.computeCRC(type + num + data, polynomial);
+        return CheckSumCalculator.computeCRC(type + num + data, polynomial);
     }
     /**
      * computes checksum and adds bitstuffing
      * @return this CharFrame as a string ready to be sent
      */
     public String format() throws InvalidFrameException{
-	if(num == null)
-	    throw new InvalidFrameException();
-	return FLAG + BitStuffer.stuff(type + num + data + computeCRC()) + FLAG;
+        if(num == null)
+            throw new InvalidFrameException();
+        return FLAG + BitStuffer.stuff(type + num + data + computeCRC()) + FLAG;
+    }
+
+    //TODO: debug
+    public boolean isValid() {
+        // String crc = CheckSumCalculator.computeCRC(type + num + data, polynomial);
+        // return CheckSumCalculator.validate(crc, polynomial);
+        return true;
     }
 
     //debug
     public static void main(String[] args) {
-	try {
-	    CharFrame cf = new CharFrame("011111100100000100000001001011101101110001111110", Sender.CRC_CCITT);
-	} catch (Exception e) {
-	    System.out.println(e);
-	}
+        try {
+            // CharFrame cf = new CharFrame("011111100100000100000001001011101101110001111110", Sender.CRC_CCITT);
+            CharFrame foo = new CharFrame('I', "test", Sender.CRC_CCITT);
+            String data = foo.getData();
+            System.out.println(data);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
